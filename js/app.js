9c85fbdcffa3b30d3a508a9af1dc3014aca64ede
Samantha Hankins <samanthahankins@MacBook-Pro-4.local>
2018-03-27 19:12:17 -0400

    Simple To-Do: DOM Scripting - Events, Part 2

--- js/app.js
+++ js/app.js
@@ -1,19 +1,66 @@
 function onReady() {
+  let toDos = [];
+  let id = 0;
   const addToDoForm = document.getElementById('addToDo');
-  const newToDoText = document.getElementById('newToDoText');
-  const taskList = document.getElementById('taskList');
+
+  function createNewTask() {
+    const newToDoText = document.getElementById('newToDoText');
+    if (!newToDoText.value) {
+      return;
+    }
+
+    toDos.push({
+      title: newToDoText.value,
+      complete: false,
+      id: id++
+    });
+
+    newToDoText.value = "";
+    renderTheUI();
+  };
+
+  function deleteTask(id) {
+    return toDos.filter(toDo => toDo.id !== id);
+  }
+
+  function renderTheUI() {
+    let taskList = document.getElementById('taskList');
+
+    taskList.textContent="";
+
+    toDos.forEach(function(toDo) {
+      let newLi = document.createElement('li');
+      let checkbox = document.createElement('input');
+      let deleteButton = document.createElement('button');
+
+
+      checkbox.type = 'checkbox';
+
+      newLi.textContent = toDo.title;
+
+      deleteButton.textContent = "Delete";
+
+      taskList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+      newLi.appendChild(deleteButton);
+
+      deleteButton.addEventListener('click', () => {
+        toDos = deleteTask(toDo.id);
+        renderTheUI();
+      });
+
+    });
+  }
 
   addToDoForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    let task = newToDoText.value;
-    let newLi = document.createElement('li');
-    let checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    newLi.textContent = task;
-    newLi.appendChild(checkbox);
-    taskList.appendChild(newLi);
-    newToDoText.value = "";
+    createNewTask();
+    renderTheUI();
   });
+
+
+  renderTheUI();
+
 }
 
 window.onload = function() {
